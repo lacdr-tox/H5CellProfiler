7bc6479a33318edf31b2e7a33bfd16c9862e6f68
Gerhard Burger <burger.ga@gmail.com>
2016-05-01 15:48:04 +0200

    Add some git utils

---
+++ utils/git.R
@@ -0,0 +1,12 @@
+library(git2r)
+
+getGitInformation <- function(directory) {
+  tryCatch({
+    repo <- repository(directory)
+  }, error = function(e) {
+    stop(paste(normalizePath(directory), "is not a git repository"))
+  })
+  summary <- capture.output(show(repo))
+  diff <- diff(repo, as_char = TRUE)
+  return(list('summary' = summary, 'diff' = diff))
+}
