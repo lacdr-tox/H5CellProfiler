bb3f0a4832cf057088d24ad1041bacecd635ba49
Gerhard Burger <burger.ga@gmail.com>
2018-05-17 17:39:40 +0200

    Fix case when working from downloaded zip

--- utils/git.R
+++ utils/git.R
@@ -1,11 +1,16 @@
 library(git2r)
 
 getGitInformation <- function(directory) {
+  is.repo <- T
   tryCatch({
     repo <- repository(directory)
   }, error = function(e) {
-    stop(paste(normalizePath(directory), "is not a git repository"))
+    #stop(paste(normalizePath(directory), "is not a git repository"))
+    is.repo <<- F
   })
+  if(!is.repo) {
+    return(list('summary' = "Not a git repository", 'branch' = '', 'diff' = ''))
+  }
   summary <- capture.output(show(repo))
   branch <- head(repo)@name
   diff <- diff(repo, as_char = TRUE)
